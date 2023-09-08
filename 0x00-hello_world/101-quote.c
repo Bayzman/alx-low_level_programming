993c1e41f718b2425c393d94ab89abfa6b62376a
bayzman <bassmaninno102@gmail.com>
2023-09-07 23:29:07 -0700

    edit 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*
- * Program to print to the standard error
+/**
+ * main - This is the entry point of the C program
+ * Return: Specifies the return value for the program.
  */
 
 int main(void)
 {
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-  return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
