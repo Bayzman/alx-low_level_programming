a149ff03032a105d7924347f96811889c937e6f2
bayzman <bassmaninno102@gmail.com>
2023-09-07 21:20:21 -0700

    print using the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * "Program to print Programming is like building a multilingual puzzle
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
