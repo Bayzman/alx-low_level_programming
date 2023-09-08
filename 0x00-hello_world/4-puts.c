ddeef68889189b67d1199386b548dc943dbf782c
bayzman <bassmaninno102@gmail.com>
2023-09-07 23:19:45 -0700

    edit 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,8 @@
 
 /**
  * "Program to print Programming is like building a multilingual puzzle
+ * main - This is the entry point of the C program
+ * Return: Specifies the return value for the program.
  */
 
 int main(void)
