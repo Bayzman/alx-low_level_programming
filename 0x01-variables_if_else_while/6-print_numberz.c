f2f82817e87f2c44647a786701035d43c8a5cf0f
bayzman <bassmaninno102@gmail.com>
2023-09-08 21:58:20 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * return - return value
+ */
+int main()
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+	return 0;
+}
