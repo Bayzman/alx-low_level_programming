ee9862461f99df02957ec2b832d6d7c519ff5c00
bayzman <bassmaninno102@gmail.com>
2023-09-09 02:37:52 -0700

    add README

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,15 @@
 
 int main(void)
 {
-        char alphabet;
+	char alphabet;
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-                putchar(alphabet);
+		putchar(alphabet);
         }
 	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 	{
 		putchar(alphabet);
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
