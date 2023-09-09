9b4dc2655142d35ea5f4018825227d955ae4cb83
bayzman <bassmaninno102@gmail.com>
2023-09-09 09:41:09 -0700

    add README

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,9 +2,9 @@
 
 /**
  * main - entry point
- * return - return value
+ * Return - return value
  */
-int main()
+int main(void)
 {
 	int digit;
 
@@ -12,5 +12,5 @@ int main()
 		printf("%d", digit);
 	printf("\n");
 
-	return 0;
+	return (0);
 }
