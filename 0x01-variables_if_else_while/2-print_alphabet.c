a6fddc6962902424f4eb12c3181134b509948502
bayzman <bassmaninno102@gmail.com>
2023-09-08 21:42:14 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * return - return value
+ */
+
+int main()
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+
+	return 0;
+}
