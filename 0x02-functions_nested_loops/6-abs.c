fb754aa768b651c927f92259d9dd3c1d63500169
bayzman <bassmaninno102@gmail.com>
2023-09-12 21:28:55 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - entry point
+ * @n: int to check
+ * Return: return absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
