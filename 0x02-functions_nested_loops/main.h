d7fb767079ab9beea8ee968612f7d75b62d5749d
bayzman <bassmaninno102@gmail.com>
2023-09-13 21:37:13 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,5 +9,5 @@ int print_last_digit(int i);
 void jack_bauer(void);
 void times_table(void);
 int add(int i, int j);
-void print_to_98(int i);
+void print_to_98(int n);
 void print_times_table(int i);
