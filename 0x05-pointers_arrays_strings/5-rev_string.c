5318c8c4dcff78cd62bb5a534d2ed54fdd13c7f8
bayzman <bassmaninno102@gmail.com>
2023-09-19 21:50:35 -0700

    print reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,15 +2,25 @@
 
 /**
  * rev_string - reverse a string
- * @*s: point to a char array
+ * @s: point to a char array
  * Return: void
  */
 
 void rev_string(char *s)
 {
 	int i;
+	int j;
+	int k;
+	char c;
 
 	for (i = 0; s[i] != '\0'; i++)
 		continue;
 
-	
+	k = i;
+	for (i--, j = 0; j < k / 2; i--, j++)
+	{
+		c = s[j];
+		s[j] = s[i];
+		s[i] = c;
+	}
+}
