4cc90b0702cc7046ba6165951a7b819c4754072f
bayzman <bassmaninno102@gmail.com>
2023-09-19 23:33:42 -0700

    print half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,6 +15,15 @@ void puts_half(char *str)
 
 	length = i;
 
+	if (length % 2 != 0)
+	{
+		for (i = (length - 1) / 2; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
+
 	for (i = length / 2; i < length; i++)
 	{
 		_putchar(str[i]);
