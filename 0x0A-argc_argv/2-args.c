462736ee75894cf9b7630a59aeebcfe9c0e544cc
bayzman <bassmaninno102@gmail.com>
2023-10-03 00:11:30 -0700

    print all arguments, ending with a new line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print all arguments including the first one ending with a new line
+ * @argc: count of arguments
+ * @argv: 1D vector of arguments
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	while(argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+
+	return (0);
+}
