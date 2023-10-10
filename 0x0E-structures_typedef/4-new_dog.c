7f831a51310b4349e1695c9a778e90c9a0f4c114
bayzman <bassmaninno102@gmail.com>
2023-10-10 14:51:13 -0700

    define dog_t *new_dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * dog_t new_dog - create a new dog
+ * @name: char array
+ * @age: float
+ * @owner: char array
+ * Return: dog_t
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_d;
+
+	new_d = malloc(sizeof(dog_t));
+
+	if (new_d == NULL)
+		return (NULL);
+
+	new_d->name = name;
+	new_d->age = age;
+	new_d->owner = owner;
+
+	return (new_d);
+}
