d14fa743f102e5be3ad6b869d0335d05013b793c
bayzman <bassmaninno102@gmail.com>
2023-10-10 15:26:15 -0700

    free the dogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,9 +16,6 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
 /**
  * dog_t - Typedef of struct dog
  */
@@ -26,5 +23,7 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
