ab6e9dbbe2cedd8a9619df3edb3e29e0457762ec
bayzman <bassmaninno102@gmail.com>
2023-10-11 13:33:52 -0700

    macros

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,11 +14,11 @@ void print_dog(struct dog *d)
 	{
 		d->name == NULL ? printf("Name: (nil)\n") :
 			printf("Name: %s\n", d->name);
+
 		printf("Age: %f\n", d->age);
+
 		d->owner == NULL ? printf("Owner: (nil)\n") :
 			printf("Owner: %s\n", d->owner);
-		/*printf("Name: %s\nAge: %f\nOwner: %s\n", d->name,
-		  d->age, d->owner); */
 	}
 
 	if (d == NULL)
