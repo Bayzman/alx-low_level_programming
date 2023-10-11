18075000fac1495759ffa3063abba27487af2924
bayzman <bassmaninno102@gmail.com>
2023-10-11 12:35:07 -0700

    macros

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,7 +23,7 @@ struct dog
  * dog_t - Typedef of struct dog
  */
 
-typedef struct dog dog_t;
+typedef struct dog dog_t
 
 dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
