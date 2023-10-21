33366e6f9b095c025d8360fefda447fd77fef023
bayzman <bassmaninno102@gmail.com>
2023-10-20 21:22:05 -0700

    singly-linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: constant list_t
+ * Return: size_t (unsigned int)
+ */
+
+size_t print_list(const list_t *h)
+{
+    size_t num_nodes;
+    num_nodes = 0;
+
+    while (h != NULL)
+    {
+        if (h->str != NULL)
+        {
+            printf("[%d] %s\n", h->len, h->str);
+        }
+
+        else
+        {
+            printf("[0] (nil)\n");
+        }
+
+        h = h->next;
+        num_nodes++;
+    }
+
+    return (num_nodes);
+}
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: constant linked list
+ * Return: number of elements in a linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+    size_t num_elements;
+
+    num_elements = 0;
+    while (h != NULL)
+    {
+        if (h->str != NULL)
+            num_elements++;
+
+        h = h->next;
+    }
+
+    return (num_elements);
+}
