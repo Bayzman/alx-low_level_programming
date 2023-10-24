93332824e3e40b009b5aac5fd21638853d381c41
bayzman <bassmaninno102@gmail.com>
2023-10-24 06:41:49 -0700

    free linked list2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,12 +10,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *node;
 
-	while (*head != NULL)
+	while ((*head) != NULL)
 	{
 		node = (*head)->next;
 		free(*head);
 		*head = node;
 	}
 
-	*head = NULL;
+	(*head) = NULL;
 }
