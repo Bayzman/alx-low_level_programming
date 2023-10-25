d170a8a54aa9954d2eb59a40b4d962a76a95c722
bayzman <bassmaninno102@gmail.com>
2023-10-24 18:09:58 -0700

    insert new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		num_nodes++;
 	}
 
+	if (*head == NULL)
+		return (NULL);
+
 	if (num_nodes >= idx)
 	{
 		for (i = 0; i < (idx - 1); i++)
