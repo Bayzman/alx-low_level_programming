77533ccbba9dd7a12de0e1ccfd7efc7c93d84ada
bayzman <bassmaninno102@gmail.com>
2023-10-26 21:45:10 -0700

    convert to uint

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: int to be converted to binary
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+}
