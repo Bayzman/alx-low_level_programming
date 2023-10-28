1553e3ffbe31d1f12de86eee39814a3fd19b22cc
bayzman <bassmaninno102@gmail.com>
2023-10-27 21:47:39 -0700

    flip bits

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+void print_binary_helper(unsigned long int n, int first);
+
 /**
  * print_binary - prints the binary representation of a number
  * @n: int to be converted to binary
@@ -8,4 +10,9 @@
 
 void print_binary(unsigned long int n)
 {
+	if (n > 0)
+	{
+		print_binary(n >> 1);
+		_putchar((n & 1) ? '1' : '0');
+	}
 }
