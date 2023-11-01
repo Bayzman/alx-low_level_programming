50eb152b1adec0aec62b24fc609de0fac0099c1d
bayzman <bassmaninno102@gmail.com>
2023-11-01 01:00:55 -0700

    copy from one file to another

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,73 @@
+#include "main.h"
+#define BUF 1024
+#define ERR STDERR_FILENO
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int fd1;
+	int fd2;
+	ssize_t wr1;
+	ssize_t wr2;
+	char buffer[BUF];
+	mode_t mode;
+
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	if (argc != 3)
+		dprintf(ERR, "Usage: cp file_from file_to\n"), exit(97);
+
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(ERR, "Error: Can't read from file %s\n",
+			argv[2]), exit(99);
+	}
+
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (fd2 == -1)
+	{
+		dprintf(ERR, "Error: Can't write to %s\n", argv[2]),
+			exit(99);
+	}
+
+	do {
+		wr1 = read(fd1, buffer, BUF);
+		if (wr1 == -1)
+		{
+			dprintf(ERR, "Error: Can't read from file %s\n",
+				argv[1]), exit(98);
+		}
+
+		if (wr1 > 0)
+		{
+			wr2 = write(fd2, buffer, wr1);
+			if (wr2 == -1)
+			{
+				dprintf(ERR, "Error: Can't write to %s\n",
+					argv[2]), exit(99);
+			}
+		}
+	} while (wr1 > 0);
+
+	wr1 = close(fd1);
+	if (wr1 == -1)
+	{
+		dprintf(ERR, "Error: Can't close fd %d\n", fd1),
+			exit(100);
+	}
+
+	wr2 = close(fd2);
+	if (wr2 == -1)
+	{
+		dprintf(ERR, "Error: Can't close fd %d\n", fd2),
+			exit(99);
+	}
+
+	return (0);
+}
