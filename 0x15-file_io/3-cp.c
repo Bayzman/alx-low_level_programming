023dd0122a374821143828499c0846172ea4f941
bayzman <bassmaninno102@gmail.com>
2023-11-01 01:05:57 -0700

    copy from one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,22 +21,22 @@ int main(int argc, char *argv[])
 	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	if (argc != 3)
 	{
-		dprintf(ERR, "Usage: cp file_from file_to\n"),
-			exit(97);
+		dprintf(ERR, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
 	{
-		dprintf(ERR, "Error: Can't read from file %s\n",
-			argv[2]), exit(99);
+		dprintf(ERR, "Error: Can't read from file %s\n", argv[2]);
+		exit(99);
 	}
 
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
 	if (fd2 == -1)
 	{
-		dprintf(ERR, "Error: Can't write to %s\n", argv[2]),
-			exit(99);
+		dprintf(ERR, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
 
 	do {
@@ -44,7 +44,8 @@ int main(int argc, char *argv[])
 		if (wr1 == -1)
 		{
 			dprintf(ERR, "Error: Can't read from file %s\n",
-				argv[1]), exit(98);
+				argv[1]);
+			exit(98);
 		}
 
 		if (wr1 > 0)
@@ -53,7 +54,8 @@ int main(int argc, char *argv[])
 			if (wr2 == -1)
 			{
 				dprintf(ERR, "Error: Can't write to %s\n",
-					argv[2]), exit(99);
+					argv[2]);
+				exit(99);
 			}
 		}
 	} while (wr1 > 0);
@@ -61,15 +63,15 @@ int main(int argc, char *argv[])
 	wr1 = close(fd1);
 	if (wr1 == -1)
 	{
-		dprintf(ERR, "Error: Can't close fd %d\n", fd1),
-			exit(100);
+		dprintf(ERR, "Error: Can't close fd %d\n", fd1);
+		exit(100);
 	}
 
 	wr2 = close(fd2);
 	if (wr2 == -1)
 	{
-		dprintf(ERR, "Error: Can't close fd %d\n", fd2),
-			exit(99);
+		dprintf(ERR, "Error: Can't close fd %d\n", fd2);
+		exit(99);
 	}
 
 	return (0);
