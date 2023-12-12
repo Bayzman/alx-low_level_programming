344d8d1e9dcc762d31a613f45ecad2753a5bfd34
bayzman <bassmaninno102@gmail.com>
2023-12-11 18:04:55 -0800

    C functions called from Python

---
+++ 0x18-dynamic_libraries/math_ops.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - add 2 integers
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract 2 integers
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply 2 integers
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide 2 integers
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod of 2 integers
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
