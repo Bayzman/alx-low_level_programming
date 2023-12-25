394072e5b3774fa571c091d2501261061dfa1fd9
bayzman <bassmaninno102@gmail.com>
2023-12-25 04:55:56 -0800

    update

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,19 +10,12 @@ void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *node;
 	hash_node_t *tmp;
-	unsigned long int size;
 	unsigned long int i;
 
-	size = ht->size;
 	if (ht == NULL)
 		return;
-	if (ht->array == NULL)
-	{
-		free(ht);
-		return;
-	}
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
 		node = ht->array[i];
 		while (node)
