1cf5448b01327aba8376643aeb3651ae8ae414ef
Bayzman <bassmaninno102@gmail.com>
2024-04-13 17:19:46 -0700

    delete hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -18,7 +18,7 @@ void hash_table_delete(hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		node = ht->array[i];
-		while (node)
+		while (node != NULL)
 		{
 			tmp = node;
 			free(tmp->key);
