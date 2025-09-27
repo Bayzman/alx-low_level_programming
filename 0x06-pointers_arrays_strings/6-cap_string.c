b175484ac4993bbd4a5aa3d7f4ef57ad474e2b91
Bayzman <bassmaninno102@gmail.com>
2025-09-26 19:07:13 -0700

    capitalize each word of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,6 +2,33 @@
 
 /**
  * cap_string - capitalize all words of a string
- * @str: char array
+ * @s: char array
  * Return: array of capitalized words in a string
  */
+
+char *cap_string(char *s)
+{
+	int i, j, sep_len;
+
+	char *sep = ",;.!?(){}\n\t\" ";
+
+	for (sep_len = 0; sep[sep_len] != 0; sep_len++)
+		continue;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (i == 0 && *(s + i) >= 'a' && *(s + i) <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < sep_len; j++)
+		{
+			if (s[i] == sep[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] -= 32;
+			}
+		}
+	}
+
+	return (s);
+}
