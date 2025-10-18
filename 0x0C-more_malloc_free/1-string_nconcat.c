3a0e1f40b22a946444d43a2bfee2776db945643e
Bayzman <bassmaninno102@gmail.com>
2025-10-18 12:50:02 -0700

    refactor code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,12 +13,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *concat;
 	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
 	while (s1 && s1[len1])
 		len1++;
 
@@ -30,6 +24,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	else
 		concat = malloc(sizeof(char) * (len1 + n + 1));
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
 
 	if (concat == NULL)
 		return (NULL);
